d63dc585593d0e53221174876f94dd6c8d197a1e
Hyukho Song <hyukho83@gmail.com>
2021-11-01 15:08:20 +0900

    updated

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 * [완주하지 못한 선수](hash/001-marathon-player-list-check.md)
 
-* 전화번호 목록
+* [전화번호 목록](hash/002-phone-book-prefix.md)
 
 * 위장
 
--- hash/002-phone-book-prefix.py
+++ hash/002-phone-book-prefix.py
@@ -1,3 +1,8 @@
 def solution(phone_book):
   answer = True
+  for x in phone_book:
+    if phone_book.startwith(x) == True :
+      answer = True
+    else:
+      answer = False
   return answer
